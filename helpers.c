a93f8cd4d482e06b0949efd8f59c0ba5ddc51588
nedko <nedko@6810d1d6-4e26-0410-916f-c2764fbbc54e>
2007-02-09 21:27:50 +0000

    New function: lv2dynparam_enum_duplicate()
    git-svn-id: svn://svn.gna.org/svn/lv2dynparam/code@36 6810d1d6-4e26-0410-916f-c2764fbbc54e

---
+++ helpers.c
@@ -0,0 +1,74 @@
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*****************************************************************************
+ *
+ *   This file is part of lv2dynparam libraries
+ *
+ *   Copyright (C) 2006,2007 Nedko Arnaudov <nedko@arnaudov.name>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *****************************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "types.h"
+#include "memory_atomic.h"
+#include "helpers.h"
+#include "log.h"
+
+char **
+lv2dynparam_enum_duplicate(
+  lv2dynparam_memory_handle memory,
+  const char ** values_ptr_ptr,
+  unsigned int values_count)
+{
+  unsigned int i;
+  char ** values;
+  size_t value_size;
+
+  values = lv2dynparam_memory_allocate(memory, values_count * sizeof(char *));
+  if (values == NULL)
+  {
+    LOG_DEBUG("Failed to allocate memory for enum array");
+    return NULL;
+  }
+
+  for (i = 0 ; i < values_count ; i++)
+  {
+    value_size = strlen(values_ptr_ptr[i]);
+
+    LOG_DEBUG("Allocating memory for enum array entry at index %u", i);
+    values[i] = lv2dynparam_memory_allocate(memory, value_size);
+    if (values[i] == NULL)
+    {
+      LOG_DEBUG("Failed to allocate memory for enum array entry");
+
+      while (i > 0)
+      {
+        i--;
+        LOG_DEBUG("Deallocating memory for enum array entry at index %u", i);
+        lv2dynparam_memory_deallocate(values[i]);
+      }
+
+      lv2dynparam_memory_deallocate(values);
+
+      return NULL;
+    }
+
+    memcpy(values[i], values_ptr_ptr[i], value_size);
+  }
+
+  return values;
+}
---
+++ helpers.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C ; c-basic-offset: 2 -*- */
+/*****************************************************************************
+ *
+ *   This file is part of lv2dynparam libraries
+ *
+ *   Copyright (C) 2006,2007 Nedko Arnaudov <nedko@arnaudov.name>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *****************************************************************************/
+
+#ifndef HELPERS_H__E8B13390_50AB_495C_8AA3_BE4F644C13A8__INCLUDED
+#define HELPERS_H__E8B13390_50AB_495C_8AA3_BE4F644C13A8__INCLUDED
+
+char **
+lv2dynparam_enum_duplicate(
+  lv2dynparam_memory_handle memory,
+  const char ** values,
+  unsigned int values_count);
+
+#endif /* #ifndef HELPERS_H__E8B13390_50AB_495C_8AA3_BE4F644C13A8__INCLUDED */
